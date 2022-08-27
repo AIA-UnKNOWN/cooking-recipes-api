1ed9b0945024c39bc425b15817d1e4a8e142bf12
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-08-28 02:08:36 +0800

    setup module aliases and database config, also added docker env variable

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql',
+  }
+);
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConnection();
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('module-alias/register');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 9898;
