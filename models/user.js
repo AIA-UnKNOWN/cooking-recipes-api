8ffbac10352c668fc24798f28c2ad3749c3e0813
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-09-06 02:38:35 +0800

    added associations in users model

--- models/user.js
+++ models/user.js
@@ -4,13 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      const { User, Recipe } = models;
+      User.hasMany(Recipe, {
+        sourceKey: 'id',
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE',
+      });
     }
   }
   User.init({
