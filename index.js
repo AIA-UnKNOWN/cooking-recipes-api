3b79a39a1fcf5a02e6f020ae313d4bcbd25a7976
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-09-23 16:36:48 +0800

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require('module-alias/register');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const registerRoutes = require('@routes');
 const app = express();
 const PORT = process.env.PORT || 9898;
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/videos', express.static('storage/videos'));
